5613b82e359b8abba305f8911744d69281ddac4f
Abhinavkr009 <110846192+Abhinavkr009@users.noreply.github.com>
2024-08-13 01:42:58 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+console.log( 3 + 3)
+
+console.log("Abhinav Kumar")
+
+let name = "Abhi"
+let age = 23
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+// OBJECT
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
