ac8e55dba7a5c637085d1e9a21b0d824976454eb
Abhinavkr009 <110846192+Abhinavkr009@users.noreply.github.com>
2024-08-13 01:23:22 +0000

    container config and important steps done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
 console.log("Abhinav");
+//For creating nodejs environment go to file, then view and then open Command palette
+//Or just click (ctrl+shift+p);
+//After that write container and then select Add Dev Container Configuration Files 
+//and then select create new configuration and then nodejs and javascript 
+//Then for progress save select source control option (or ctrl+shift+g)
+//then select the file by + sign and insert a message
+//After that commit and then push by 3 dots. Now the changes and files are saved in repository..
