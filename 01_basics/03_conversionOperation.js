743674d4b5cda48dfd11d7188c7a17122d45ec8a
Abhinavkr009 <110846192+Abhinavkr009@users.noreply.github.com>
2024-08-14 16:01:06 +0000

    OperationsDone

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,7 +20,7 @@ console.log(valueInNumber);
 let isLoggedIn = "Pragati"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false
@@ -29,6 +29,38 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ************************************* Operations *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+// Link to study 
+// https://tc39.es/ecma262/#sec-type-conversion
