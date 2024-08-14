a4c8c41e0bc9846a40b9069ccb18998947cc03fb
Abhinavkr009 <110846192+Abhinavkr009@users.noreply.github.com>
2024-08-14 14:10:29 +0000

    coversionDone

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "99abc"
+// let score = null // 0
+// let score = undefined // NaN
+// let score = true// 1
+/*
+let score = "Abhinav"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+// "99" => 99
+// "99abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "Pragati"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"Pragati" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
